63fcefb6098bc1798e43c6f35b2fae0d51bf8494
Smael Nicolas <nico.smael@gmail.com>
2022-08-24 17:50:53 -0300

    imports components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./app.css";
+import { ItemListContainer } from "./Components/ItemListContainer/ItemListContainer";
 import { NavbarMUI } from "./Components/Navbar/NavbarMUI";
 
 function App() {
 	return (
 		<div className='app--container'>
 			<NavbarMUI />
+			<ItemListContainer message={"HOLA!"} />
 		</div>
 	);
 }
