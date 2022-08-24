13f2d728a09274d3b12b24fc26131df1ff3455bb
Smael Nicolas <nico.smael@gmail.com>
2022-08-24 17:37:08 -0300

    navbar creation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./app.css";
+import { NavbarMUI } from "./Components/Navbar/NavbarMUI";
 
 function App() {
-	return <div className='app--container'></div>;
+	return (
+		<div className='app--container'>
+			<NavbarMUI />
+		</div>
+	);
 }
 
 export default App;
