c8202a7a7b9644f83d77544fef60f472a76ef183
Smael Nicolas <nico.smael@gmail.com>
2022-08-24 17:50:30 -0300

    NAVBAR

--- src/Components/Navbar/NavbarMUI.jsx
+++ src/Components/Navbar/NavbarMUI.jsx
@@ -1,7 +1,5 @@
-import AdbIcon from "@mui/icons-material/Adb";
 import MenuIcon from "@mui/icons-material/Menu";
 import AppBar from "@mui/material/AppBar";
-import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
@@ -9,54 +7,28 @@ import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Toolbar from "@mui/material/Toolbar";
-import Tooltip from "@mui/material/Tooltip";
-import Typography from "@mui/material/Typography";
 import React, { useState } from "react";
+import { CartWidget } from "../CartWidget/CartWidget";
 
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import "./navbarMUI.css";
+const pages = ["Remeras", "Pantalones", "Zapas"];
 
 export const NavbarMUI = () => {
 	const [anchorElNav, setAnchorElNav] = useState(null);
-	const [anchorElUser, setAnchorElUser] = useState(null);
 
 	const handleOpenNavMenu = (event) => {
 		setAnchorElNav(event.currentTarget);
-	};
-	const handleOpenUserMenu = (event) => {
-		setAnchorElUser(event.currentTarget);
 	};
 
 	const handleCloseNavMenu = () => {
 		setAnchorElNav(null);
 	};
 
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
-
 	return (
 		<AppBar position='static'>
 			<Container maxWidth='xl'>
 				<Toolbar disableGutters>
-					<Typography
-						variant='h6'
-						noWrap
-						component='a'
-						href='/'
-						sx={{
-							mr: 2,
-							display: { xs: "none", md: "flex" },
-							fontFamily: "monospace",
-							fontWeight: 700,
-							letterSpacing: ".3rem",
-							color: "inherit",
-							textDecoration: "none",
-						}}
-					>
-						✨PRACTICAS
-					</Typography>
-
+					<h2 className='navbar--title'>✨ PRACTICAS</h2>
 					<Box
 						sx={{
 							flexGrow: 1,
@@ -83,7 +55,7 @@ export const NavbarMUI = () => {
 							keepMounted
 							transformOrigin={{
 								vertical: "top",
-								horizontal: "left",
+								horizontal: "center",
 							}}
 							open={Boolean(anchorElNav)}
 							onClose={handleCloseNavMenu}
@@ -96,34 +68,11 @@ export const NavbarMUI = () => {
 									key={page}
 									onClick={handleCloseNavMenu}
 								>
-									<Typography textAlign='center'>
-										{page}
-									</Typography>
+									<div>{page}</div>
 								</MenuItem>
 							))}
 						</Menu>
 					</Box>
-					<AdbIcon
-						sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
-					/>
-					<Typography
-						variant='h5'
-						noWrap
-						component='a'
-						href=''
-						sx={{
-							mr: 2,
-							display: { xs: "flex", md: "none" },
-							flexGrow: 1,
-							fontFamily: "monospace",
-							fontWeight: 700,
-							letterSpacing: ".3rem",
-							color: "inherit",
-							textDecoration: "none",
-						}}
-					>
-						LOGO
-					</Typography>
 					<Box
 						sx={{
 							flexGrow: 1,
@@ -140,46 +89,8 @@ export const NavbarMUI = () => {
 							</Button>
 						))}
 					</Box>
-
 					<Box sx={{ flexGrow: 0 }}>
-						<Tooltip title='Open settings'>
-							<IconButton
-								onClick={handleOpenUserMenu}
-								sx={{ p: 0 }}
-							>
-								<Avatar
-									alt='Remy Sharp'
-									src='/static/images/avatar/2.jpg'
-								/>
-							</IconButton>
-						</Tooltip>
-						<Menu
-							sx={{ mt: "45px" }}
-							id='menu-appbar'
-							anchorEl={anchorElUser}
-							anchorOrigin={{
-								vertical: "top",
-								horizontal: "right",
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: "top",
-								horizontal: "right",
-							}}
-							open={Boolean(anchorElUser)}
-							onClose={handleCloseUserMenu}
-						>
-							{settings.map((setting) => (
-								<MenuItem
-									key={setting}
-									onClick={handleCloseUserMenu}
-								>
-									<Typography textAlign='center'>
-										{setting}
-									</Typography>
-								</MenuItem>
-							))}
-						</Menu>
+						<CartWidget />
 					</Box>
 				</Toolbar>
 			</Container>
