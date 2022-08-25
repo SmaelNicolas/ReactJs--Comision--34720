8e4cd3508a7285ef0fef993cbc11a8b668614e80
Smael Nicolas <nico.smael@gmail.com>
2022-08-25 13:56:29 -0300

    Catalogo de productos

---
+++ src/Data/catalogoProductos.js
@@ -0,0 +1,66 @@
+export const catalogo = [
+	{
+		id: 1,
+		title: "Producto N°1",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+		price: 500,
+		pictureUrl: "https://picsum.photos/id/237/200/300",
+	},
+	{
+		id: 2,
+		title: "Producto N°2",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+		price: 350,
+		pictureUrl: "https://picsum.photos/id/405/200/300",
+	},
+	{
+		id: 3,
+		title: "Producto N°3",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		price: 99,
+		pictureUrl: "https://picsum.photos/id/100/200/300",
+	},
+	{
+		id: 4,
+		title: "Producto N°4",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+		price: 107,
+		pictureUrl: "https://picsum.photos/id/369/200/300",
+	},
+	{
+		id: 5,
+		title: "Producto N°5",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+		price: 1028.65,
+		pictureUrl: "https://picsum.photos/id/248/200/300",
+	},
+	{
+		id: 6,
+		title: "Producto N°6",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+		price: 209,
+		pictureUrl: "https://picsum.photos/id/147/200/300",
+	},
+	{
+		id: 7,
+		title: "Producto N°7",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+		price: 746,
+		pictureUrl: "https://picsum.photos/id/25/200/300",
+	},
+	{
+		id: 8,
+		title: "Producto N°8",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+		price: 10,
+		pictureUrl: "https://picsum.photos/id/19/200/300",
+	},
+];
