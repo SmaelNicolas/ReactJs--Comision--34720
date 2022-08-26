cc7ca05970fdbca594e5432866b2985c93871191
Smael Nicolas <nico.smael@gmail.com>
2022-08-25 22:17:45 -0300

    ejemplo promise

--- src/Components/Promises/Promises.jsx
+++ src/Components/Promises/Promises.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
+//comentar este catalogo cuando se descomente catalogo = [];
 import { catalogo } from "../../Data/catalogoProductos";
 
 export const Promises = () => {
+	//descomentar esta const catalogo = [] para que la promesa sea rechazada
 	// const catalogo = [];
 	const [loading, setLoading] = useState(true);
 	const [seg, setSeg] = useState(3);
 
 	const soyUnaPromesa = new Promise((resolve, reject) => {
+		//si el catalogo tiene items, la promesa se acepta y toma el valor catalogo
+		//si el catalogo es vacio , la promesa es rechazada y nos informa que no tiene items
 		catalogo.length > 0
 			? setTimeout(() => {
 					resolve(catalogo);
@@ -27,11 +31,15 @@ export const Promises = () => {
 	}, []);
 
 	//manejo de la promesa
+	//usamos useEffect con dependencia [] para que se ejecute una sola vez al crearce el componente.
+	// Si no usaramos useEffect es probable que entremos en loops infinitos cada vez que un estado se modifique
 	useEffect(() => {
 		soyUnaPromesa
-			//then es cuando la promesa es aceptada , data es el valor del resolve
+			//then es para cuando la promesa es aceptada , data es el valor del resolve. Aca DATA = CATALOGO
+			//Aca es donde se darian los valores a los states creados para cuando la promesa es aceptada
 			.then((data) => console.log(data))
-			//err es cuando la promesa es rechazada , err es el valor del reject
+			//err es para cuando la promesa es rechazada , err es el valor del reject. Aca ERR = "NO HAY PRODUCTOS"
+			//Aca es donde se darian los valores a los states creados para cuando la promesa es rechazada
 			.catch((err) => console.log(err))
 			//no importa el resultado , siempre ejecuta finally
 			.finally(
@@ -39,12 +47,11 @@ export const Promises = () => {
 					setLoading(false);
 				}, 3000)
 			);
-
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	return (
 		<div>
+			{/* Muestra el cargando datos hasta que entra en el finally. Para simular el retraso tmb usa un setTimeout el loading */}
 			{loading ? (
 				<h1>CARGANDO DATOS ... {seg}</h1>
 			) : (
