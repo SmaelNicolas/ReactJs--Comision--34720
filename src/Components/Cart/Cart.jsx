2a6ad92d849edca2ada21d85ebd417995a97d729
Smael Nicolas <nico.smael@gmail.com>
2022-09-19 22:30:45 -0300

    DESAFIO CART VIEW

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { CartContext } from "../../Context/CartContext";
 import { Loading } from "../Loading/Loading";
 import "./cart.css";
+import { ItemCard } from "./ItemCard";
 
 export const Cart = () => {
 	const [loading, setLoading] = useState(true);
 
-	const { quantityItems } = useContext(CartContext);
+	const { cart, quantityItems, clearCart } = useContext(CartContext);
 
 	const itemsInCart = quantityItems();
 
@@ -18,9 +20,21 @@ export const Cart = () => {
 
 	return loading ? (
 		<Loading />
+	) : itemsInCart === 0 ? (
+		<div className='cart--empty'>
+			<div className='cart--empty--text'>CARRITO VACIO</div>
+			<Link className='cart--empty--link' to='/items'>
+				Ir a Comprar
+			</Link>
+		</div>
 	) : (
 		<div className='cart--container'>
-			<div>Total : {itemsInCart}</div>
+			<button className='cart--emptyButton' onClick={() => clearCart()}>
+				VACIAR CARRITO
+			</button>
+			{cart.map((item) => (
+				<ItemCard item={item} />
+			))}
 		</div>
 	);
 };
---
+++ src/Components/Cart/ItemCard.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { BsTrash } from "react-icons/bs";
+import { CartContext } from "../../Context/CartContext";
+import "./itemCard.css";
+
+export const ItemCard = ({ item }) => {
+	const { removeItem } = useContext(CartContext);
+
+	return (
+		<div className='itemCart--card'>
+			<div className='itemCart--card--title'>{item.title}</div>
+			<img
+				className='itemCart--card--img'
+				src={item.pictureUrl}
+				alt={item.title}
+				srcset=''
+			/>
+			<div className='itemCart--card--text'>
+				Cantidad : {item.quantity}
+			</div>
+			<div className='itemCart--card--text'>
+				Precio Total : {item.quantity * item.price}
+			</div>
+			<BsTrash
+				className='itemCart--card--trash'
+				onClick={() => removeItem(item.id)}
+			/>
+		</div>
+	);
+};
